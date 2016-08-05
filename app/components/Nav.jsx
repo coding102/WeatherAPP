6362790b35fa19b943a007811beabe7768762846
Marck <mercercountynj@gmail.com>
2016-08-05 00:23:41 -0400

    added examples component

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var {Link} = require('react-router');
 var Nav = React.createClass({
     render: function() {
         return (
-            <h2>Nav Component</h2>
+            <div>
+                <h2>Nav Component</h2>
+                <Link to="/">Get Weather</Link>
+                <Link to="/about">About</Link>
+                <Link to="/examples">Examples</Link>
+
+            </div>
         );
     }
 });
