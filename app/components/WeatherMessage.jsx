54326b9a4ae877934469addc26e0c0613591b8e5
Marck <mercercountynj@gmail.com>
2016-08-15 21:11:13 -0400

    further destructuring

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -10,8 +10,7 @@ var React = require('react');
 //     }
 // });
 
-var WeatherMessage = (props) => {
-    var {temp, location} = props;
+var WeatherMessage = ({temp, location}) => {
     return (
         <h3>It is {temp} in {location}</h3>
     )
