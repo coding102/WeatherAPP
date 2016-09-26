41b516018ec8811cbd8f49645f61f5fed4c93c2f
Marck <mercercountynj@gmail.com>
2016-09-25 23:05:00 -0400

    fixes

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,23 +1,13 @@
 var React = require('react');
 var {Link} = require('react-router');
 
-// var Examples = React.createClass({
-//     render: function() {
-//         return (
-//             <h3>Examples Component</h3>
-//         );
-//     }
-// });
-
 var Examples = (props) => {
     return (
         <div>
             <h1 className="text-center page-title">Example</h1>
             <p>Here are a few locations to try out:</p>
             <ol>
-                <li>
-                    <Link to='/?location=Princeton'>Princeton, NJ</Link>
-                </li>
+                
                 <li>
                     <Link to='/?location=Austin'>Austin, TX</Link>
                 </li>
